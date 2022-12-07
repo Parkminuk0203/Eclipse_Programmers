454d9ad0be8a6d4786c307bedcbb6a25535c4fb3
Minuk Park <parkminuk0203@gmail.com>
2022-12-07 20:53:20 +0900

    나이 출력

---
+++ Step0/src/Programmers/나이_출력.java
@@ -0,0 +1,15 @@
+package Programmers;
+
+public class 나이_출력 {
+
+	public static void main(String[] args) {
+		System.out.println(solution(40));
+	} // end of main
+	public static int solution(int age) {
+		int answer = 0;
+		
+		answer = 2023 - age;
+		
+		return answer;
+	}
+} // end of class
