085918a5e6fddfad7f0da42c5403dc7faf7464ac
Minuk Park <parkminuk0203@gmail.com>
2022-12-07 17:16:15 +0900

    test

---
+++ Step0/src/Programmers/옹알이.java
@@ -0,0 +1,10 @@
+package Programmers;
+
+public class ¿Ë¾ËÀÌ {
+
+	public static void main(String[] args) {
+		System.out.println("¹Ú¹Î¿í");
+
+	}
+
+}
---
+++ Step0/src/Programmers/옹알이2.java
@@ -0,0 +1,9 @@
+package Programmers;
+
+public class ¿Ë¾ËÀÌ2 {
+
+	public static void main(String[] args) {
+		System.out.println("test");
+	}
+
+}
