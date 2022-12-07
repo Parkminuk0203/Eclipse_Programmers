cde5eacdc18c9209db02ec30258dd9728cb3e237
Minuk Park <parkminuk0203@gmail.com>
2022-12-07 20:36:26 +0900

    프로그래머스

---
+++ Step0/src/Programmers/몫_구하기.java
@@ -0,0 +1,20 @@
+package Programmers;
+
+public class ¸ò_±¸ÇÏ±â {
+
+	public static void main(String[] args) {
+		System.out.println(solution(10,5));
+		
+	} // end of main
+	public static int solution(int num1, int num2) {
+		int answer = 0;
+		int divide = 0;
+		
+		divide = num1 / num2;
+		
+		answer = divide;
+		
+		
+		return answer;
+	}
+} // end of class
--- Step0/src/Programmers/연속된_수의_합.java
+++ Step0/src/Programmers/연속된_수의_합.java
@@ -8,6 +8,10 @@ public class 연속된_수의_합 {
 	public static int[] solution(int num, int total) {
 		int[] answer = new int[num]; // 매개변수 num으로 배열 사이즈 정함
 		
+		int nsum = num*(1+num)/2;
+		int start = (total - nsum) / num;
+		
+		
 		
 		return answer;
 	}
