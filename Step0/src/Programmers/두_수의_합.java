7b4ecff519580460c03776466836d5643439f314
Minuk Park <parkminuk0203@gmail.com>
2022-12-07 20:55:05 +0900

    두 수의 합

---
+++ Step0/src/Programmers/두_수의_합.java
@@ -0,0 +1,17 @@
+package Programmers;
+
+public class 두_수의_합 {
+
+	public static void main(String[] args) {
+		System.out.println(solution(3,4));
+	} // end of main
+	public static int solution(int num1, int num2) {
+		int answer = 0;
+		int sum = 0;
+		
+		sum = num1 + num2;
+		answer = sum;
+		
+		return answer;
+	}
+} // end of class
