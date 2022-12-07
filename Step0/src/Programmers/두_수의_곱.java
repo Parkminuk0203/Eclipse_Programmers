15809f82cfe94668f86f37be0e46da4e8c88ad10
Minuk Park <parkminuk0203@gmail.com>
2022-12-07 20:47:13 +0900

    두 수의 곱

--- Step0/src/Programmers/두_수의_곱.java
+++ Step0/src/Programmers/두_수의_곱.java
@@ -3,7 +3,7 @@ package Programmers;
 public class µÎ_¼öÀÇ_°ö {
 
 	public static void main(String[] args) {
-		System.out.println(solution(2,3));
+		System.out.println(solution(3,4));
 	} // end of main
 	public static int solution(int num1, int num2) {
 		int answer = 0;
