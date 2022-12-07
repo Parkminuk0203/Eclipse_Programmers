a64dcdafbe5db1d01dca37566946493866f70da4
Minuk Park <parkminuk0203@gmail.com>
2022-12-07 20:49:30 +0900

    숫자 비교하기

---
+++ Step0/src/Programmers/숫자_비교하기.java
@@ -0,0 +1,20 @@
+package Programmers;
+
+public class 숫자_비교하기 {
+
+	public static void main(String[] args) {
+		System.out.println(solution(3,3));
+	} // end of main
+	public static int solution(int num1, int num2) {
+		int answer = 0;
+		
+		if(num1 == num2) {
+			answer = -1;
+		}
+		else {
+			answer = 1;
+		}
+		
+		return answer;
+	}
+} // end of class
