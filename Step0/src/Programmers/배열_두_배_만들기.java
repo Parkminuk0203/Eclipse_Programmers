9688c8da8c0d477e21bbec6c0b2fb719cf688ef2
Minuk Park <parkminuk0203@gmail.com>
2022-12-08 18:59:36 +0900

    배열 두 배 만들기

---
+++ Step0/src/Programmers/배열_두_배_만들기.java
@@ -0,0 +1,17 @@
+package Programmers;
+import java.util.*;
+public class 배열_두_배_만들기 {
+
+	public static void main(String[] args) {
+		int[] arr = new int[] {1,2,3,4,5};
+		int[] arr2 = new int[] {1,2,100,-99,1,2,3};
+		
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = arr[i]*2;
+			arr2[i] = arr2[i]*2;
+		}
+		System.out.println(Arrays.toString(arr));
+		System.out.println(Arrays.toString(arr2));
+	} // end of main
+	
+} // end of class
